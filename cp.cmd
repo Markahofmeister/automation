4d1ef8c0a86bb15e4ebbce61e0bc5dcb1aa9b942
markahofmeister <markhof12@gmail.com>
2021-12-16 20:25:12 -0500

    add to cp

--- cp.cmd
+++ cp.cmd
@@ -5,21 +5,32 @@ REM  - Creates a new remote repository on Github
 REM  - Clones the remote repository to the local machine
 REM  - renames item list to project-specific name
 REM  - Creates a media directory, fab files directory, code directory, subdirectories 
+REM  - Moves runPlant.cmd to flowcharts folder 
 REM  - Adds c+p.cmd to repo and .gitignore
+REM  - Commits & pushes Initial Files
 
 git remote remove origin
-gh repo create %1 --template new-project-template --public --confirm
-mkdir C:\Users\marka\Downloads\Lab\%1
-gh repo clone Markahofmeister/%1 C:\Users\marka\Downloads\Lab\%1
-cd C:\Users\marka\Downloads\Lab\%1
-rename Item-List.xlsx %1-Item-List.xlsx
-mkdir media fab-files software
+REM  - Creates a new remote repository on Github 
+gh repo create %1 --template new-project-template --public --confirm	
+mkdir C:\Lab\%1
+REM - Clones the remote repository to the local machine
+gh repo clone Markahofmeister/%1 C:\Lab\%1								
+cd C:\Lab\%1
+REM  - renames item list to project-specific name
+rename Item-List.xlsx %1-Item-List.xlsx			
+REM  - Creates a media directory, fab files directory, code directory, subdirectories 						
+mkdir media fab-files software											
 cd media
 mkdir flowcharts pinouts-datasheets system-diagrams images 
-cd ..
-echo c+p.cmd > .gitignore
+move C:\Lab\%1\runPlant.cmd C:\Lab\%1\media\flowcharts\					
+cd C:\Lab\%1
+REM  - Adds c+p.cmd to .gitignore
+echo c+p.cmd > .gitignore								
+REM  - Adds plantuml.jar to .gitignore 				
+echo plantuml.jar >> .gitignore											
 git add .
-git commit -m "Initial File Commit" 
+REM  - Commits Initial Files
+git commit -m "Initial File Commit" 									
 git push origin main 
 cd C:\Users\marka\Downloads\Lab\automation 
 git remote set-url origin https://github.com/Markahofmeister/automation.git
