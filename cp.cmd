000e9cdd556cb1dc595f17fe559547b2e09356f8
markahofmeister <markhof12@gmail.com>
2021-12-18 10:24:59 -0500

    plantuml to cp

--- cp.cmd
+++ cp.cmd
@@ -5,7 +5,7 @@ REM  - Creates a new remote repository on Github
 REM  - Clones the remote repository to the local machine
 REM  - renames item list to project-specific name
 REM  - Creates a media directory, fab files directory, code directory, subdirectories 
-REM  - Moves runPlant.cmd to flowcharts folder 
+REM  - Moves runPlant.cmd, plantuml.jar, flowsheet-v1 to flowcharts folder 
 REM  - Adds c+p.cmd to repo and .gitignore
 REM  - Commits & pushes Initial Files
 
@@ -17,22 +17,27 @@ REM - Clones the remote repository to the local machine
 gh repo clone Markahofmeister/%1 C:\Lab\%1								
 cd C:\Lab\%1
 REM  - renames item list to project-specific name
-rename Item-List.xlsx %1-Item-List.xlsx			
+rename BOM.xlsx %1-BOM.xlsx			
 REM  - Creates a media directory, fab files directory, code directory, subdirectories 						
 mkdir media fab-files software											
 cd media
 mkdir flowcharts pinouts-datasheets system-diagrams images 
-move C:\Lab\%1\runPlant.cmd C:\Lab\%1\media\flowcharts\					
+REM  - Moves flowchart creation scripts to flowchart folder
+move C:\Lab\%1\runPlant.cmd C:\Lab\%1\media\flowcharts\		
+move C:\Lab\%1\plantuml.jar C:\Lab\%1\media\flowcharts\			
+move C:\Lab\%1\flowsheet-v1.txt C:\Lab\%1\media\flowcharts\	
+REM  - Runs plantuml on flowsheet-v1
+java -jar plantuml.jar "flowsheet-v1.txt"
 cd C:\Lab\%1
 REM  - Adds c+p.cmd to .gitignore
 echo c+p.cmd > .gitignore								
-REM  - Adds plantuml.jar to .gitignore 				
-echo plantuml.jar >> .gitignore											
+REM  - Adds plantuml.jar (in all directories) to .gitignore 				
+echo **/plantuml.jar >> .gitignore											
 git add .
 REM  - Commits Initial Files
 git commit -m "Initial File Commit" 									
 git push origin main 
-cd C:\Users\marka\Downloads\Lab\automation 
+cd C:\Lab\automation 
 git remote set-url origin https://github.com/Markahofmeister/automation.git
 git push --set-upstream origin main 
 
